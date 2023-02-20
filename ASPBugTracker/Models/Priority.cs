f7f31fa972a929670d853e2b2077129f4d98df1e
Mukul Hooda <Mukul021>
2023-02-19 21:10:57 -0500

    Added Priority and Status Model Classes

---
+++ ASPBugTracker/Models/Priority.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPBugTracker.Models
+{
+    public class Priority
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
