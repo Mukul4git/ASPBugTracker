e8987f6f545e540706b479f4aa0d64f2ba05d387
Mukul Hooda <Mukul021>
2023-02-19 21:05:18 -0500

    Added Bug Model Class

---
+++ ASPBugTracker/Models/bug.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPBugTracker.Models
+{
+    public class bug
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string status { get; set; }
+
+        [Required]
+        public string type { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        public string priority { get; set; }
+        public string solution { get; set; }
+        
+        [Required]
+        public string creator { get; set; }
+
+
+    }
+}
